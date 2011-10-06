c4c4e88e3d79e0ae6e552e0b6f9cd6513b2cd32d
Federico Cargnelutti <fedecarg@gmail.com>
2011-10-06 10:31:03 +0100

    converted tabs to spaces

--- jquery.htmlpaginate.js
+++ jquery.htmlpaginate.js
@@ -31,78 +31,78 @@
  */
 (function($) {
 	
-	$.fn.htmlpaginate = function(options) {
-		
-	    var HtmlPaginator = function(self, options) {
-	    	
-	        var defaults = {
-	            itemsPerPage: 10,
-	            selector: {
-	            	next: self.selector+'-next',
-	            	previous: self.selector+'-previous',
-	            	pagination: self.selector+'-pagination'
-	            }
-	        };
-	        var options = $.extend(defaults, options);
-	        var currentPage = 1;
-	        var numberOfPages = 1;
-	        var numberOfItems = 0;
-	        
-	        var init = function() {
-	            numberOfItems = self.children().size();
-	            numberOfPages = Math.ceil(numberOfItems / options.itemsPerPage);
-	            if (numberOfPages > 1) {
-	                $(options.selector.pagination).show();
-	                $(options.selector.previous).removeAttr('href');
-	            }
-	            
-	            self.children().hide();
-	            self.children().slice(0, options.itemsPerPage).show();
-	            
-	            $(options.selector.previous).click(function(e){
-	                e.preventDefault();
-	                previous();
-	            });
-	            $(options.selector.next).click(function(e){
-	                e.preventDefault();
-	                next();
-	            });
-	            
-	            self.show();
-	        }
-	        
-	        var show = function(page) {            
-	            startPage = (page - 1) * options.itemsPerPage;
-	            endPage = startPage + options.itemsPerPage;
-	            self.children().hide().slice(startPage, endPage).show();
-	            
-	            $(options.selector.previous).attr('href', '#');
-	            $(options.selector.next).attr('href', '#');
-	            if (page <= 1) {
-	                $(options.selector.previous).removeAttr('href');
-	            } else if (page == numberOfPages) {
-	                $(options.selector.next).removeAttr('href');
-	            }
-	            
-	            currentPage = page;
-	        };
-	        
-	        var next = function() {
-	            if (currentPage < numberOfPages){
-	                show(currentPage + 1);
-	            }
-	        };
-	        
-	        var previous = function() {
-	            if (currentPage > 1) {
-	                show(currentPage - 1);
-	            }
-	        };
-	        
-	        init();
-	        return this;
-	    }
-	    
-	    return new HtmlPaginator(this, options);
-    };
+$.fn.htmlpaginate = function(options) {
+	
+    var HtmlPaginator = function(self, options) {
+    	
+        var defaults = {
+            itemsPerPage: 10,
+            selector: {
+            	next: self.selector+'-next',
+            	previous: self.selector+'-previous',
+            	pagination: self.selector+'-pagination'
+            }
+        };
+        var options = $.extend(defaults, options);
+        var currentPage = 1;
+        var numberOfPages = 1;
+        var numberOfItems = 0;
+        
+        var init = function() {
+            numberOfItems = self.children().size();
+            numberOfPages = Math.ceil(numberOfItems / options.itemsPerPage);
+            if (numberOfPages > 1) {
+                $(options.selector.pagination).show();
+                $(options.selector.previous).removeAttr('href');
+            }
+            
+            self.children().hide();
+            self.children().slice(0, options.itemsPerPage).show();
+            
+            $(options.selector.previous).click(function(e){
+                e.preventDefault();
+                previous();
+            });
+            $(options.selector.next).click(function(e){
+                e.preventDefault();
+                next();
+            });
+            
+            self.show();
+        }
+        
+        var show = function(page) {            
+            startPage = (page - 1) * options.itemsPerPage;
+            endPage = startPage + options.itemsPerPage;
+            self.children().hide().slice(startPage, endPage).show();
+            
+            $(options.selector.previous).attr('href', '#');
+            $(options.selector.next).attr('href', '#');
+            if (page <= 1) {
+                $(options.selector.previous).removeAttr('href');
+            } else if (page == numberOfPages) {
+                $(options.selector.next).removeAttr('href');
+            }
+            
+            currentPage = page;
+        };
+        
+        var next = function() {
+            if (currentPage < numberOfPages){
+                show(currentPage + 1);
+            }
+        };
+        
+        var previous = function() {
+            if (currentPage > 1) {
+                show(currentPage - 1);
+            }
+        };
+        
+        init();
+        return this;
+    }
+    
+    return new HtmlPaginator(this, options);
+};
 })(jQuery);
