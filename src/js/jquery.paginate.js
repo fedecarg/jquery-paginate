24ca949b93e8bba7afefeeaa8ecf8fe9a3849961
Federico Cargnelutti <fedecarg@gmail.com>
2025-08-01 16:40:21 +0100

    Update paginate.js

--- src/js/jquery.paginate.js
+++ src/js/jquery.paginate.js
@@ -1,13 +1,10 @@
 /**
- * Paginate - jQuery Plugin
- * By Federico Cargnelutti <fedecarg@gmail.com>
+ * Paginate - Vanilla JavaScript Plugin
+ * Converted from jQuery version by Federico Cargnelutti <fedecarg@gmail.com>
  * 
  * Dual licensed under the MIT and GPL licenses:
  *   - http://www.opensource.org/licenses/mit-license.php
  *   - http://www.gnu.org/licenses/gpl.html
- * 
- * Examples and documentation at: 
- *   - http://github.com/fedecarg/jquery-htmlpaginate
  * 
  * Usage:
  * 
@@ -23,88 +20,150 @@
  *     <a id="items-previous" href="#">&laquo; Previous</a> 
  *     <a id="items-next" href="#">Next &raquo;</a>
  * </div>
- *     
- * <script type="text/javascript">
- * $('#items').paginate({itemsPerPage: 2});
- * </script>
  * 
+ * Usage examples:
+ *   Method 1 - Using prototype method:
+ *   document.getElementById('items').paginate({itemsPerPage: 2});
+ *
+ *   Method 2 - Using standalone function:
+ *   paginate(document.getElementById('items'), {itemsPerPage: 2});
  */
-(function($) {
+
+// Add paginate method to HTMLElement prototype
+HTMLElement.prototype.paginate = function(options = {}) {
     
-$.fn.paginate = function(options) {
-    
-    var Paginator = function(self, options) {
+    const Paginator = function(element, options) {
         
-        var defaults = {
+        const defaults = {
             itemsPerPage: 10,
             selector: {
-                next: self.selector+'-next',
-                previous: self.selector+'-previous',
-                pagination: self.selector+'-pagination'
+                next: element.id + '-next',
+                previous: element.id + '-previous',
+                pagination: element.id + '-pagination'
             },
             cssClassName: {
                 disabled: 'disabled'
             }
         };
-        var options = $.extend(defaults, options);
-        var currentPage = 1;
-        var numberOfPages = 1;
-        var numberOfItems = 0;
         
-        var init = function() {
-            numberOfItems = self.children().size();
-            numberOfPages = Math.ceil(numberOfItems / options.itemsPerPage);
+        // Merge options with defaults
+        const config = Object.assign({}, defaults, options);
+        let currentPage = 1;
+        let numberOfPages = 1;
+        let numberOfItems = 0;
+        
+        const init = function() {
+            numberOfItems = element.children.length;
+            numberOfPages = Math.ceil(numberOfItems / config.itemsPerPage);
+            
             if (numberOfPages > 1) {
-                $(options.selector.pagination).show();
-                $(options.selector.previous).addClass(options.cssClassName.disabled);
+                const paginationEl = document.getElementById(config.selector.pagination);
+                if (paginationEl) {
+                    paginationEl.style.display = 'block';
+                }
+                
+                const previousEl = document.getElementById(config.selector.previous);
+                if (previousEl) {
+                    previousEl.classList.add(config.cssClassName.disabled);
+                }
             }
             
-            self.children().hide();
-            self.children().slice(0, options.itemsPerPage).show();
-            
-            $(options.selector.previous).click(function(e){
-                e.preventDefault();
-                previous();
-            });
-            $(options.selector.next).click(function(e){
-                e.preventDefault();
-                next();
+            // Hide all children initially
+            Array.from(element.children).forEach(child => {
+                child.style.display = 'none';
             });
             
-            self.show();
-        }
+            // Show first page items
+            Array.from(element.children).slice(0, config.itemsPerPage).forEach(child => {
+                child.style.display = 'block';
+            });
+            
+            // Add event listeners
+            const previousEl = document.getElementById(config.selector.previous);
+            const nextEl = document.getElementById(config.selector.next);
+            
+            if (previousEl) {
+                previousEl.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    previous();
+                });
+            }
+            
+            if (nextEl) {
+                nextEl.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    next();
+                });
+            }
+            
+            element.style.display = 'block';
+        };
         
-        var show = function(page) {
+        const show = function(page) {
             currentPage = page;
-            startPage = (currentPage - 1) * options.itemsPerPage;
-            endPage = startPage + options.itemsPerPage;
-            self.children().hide().slice(startPage, endPage).show();
-
-            var disabled = options.cssClassName.disabled;
-            $(options.selector.pagination + ' a').removeClass(disabled);
-            if (currentPage <= 1) {
-                $(options.selector.previous).addClass(disabled);
-            } else if (currentPage == numberOfPages) {
-                $(options.selector.next).addClass(disabled);
+            const startPage = (currentPage - 1) * config.itemsPerPage;
+            const endPage = startPage + config.itemsPerPage;
+            
+            // Hide all children
+            Array.from(element.children).forEach(child => {
+                child.style.display = 'none';
+            });
+            
+            // Show current page items
+            Array.from(element.children).slice(startPage, endPage).forEach(child => {
+                child.style.display = 'block';
+            });
+            
+            // Update navigation button states
+            const disabled = config.cssClassName.disabled;
+            const paginationEl = document.getElementById(config.selector.pagination);
+            
+            if (paginationEl) {
+                const links = paginationEl.querySelectorAll('a');
+                links.forEach(link => {
+                    link.classList.remove(disabled);
+                });
+            }
+            
+            const previousEl = document.getElementById(config.selector.previous);
+            const nextEl = document.getElementById(config.selector.next);
+            
+            if (currentPage <= 1 && previousEl) {
+                previousEl.classList.add(disabled);
+            } else if (currentPage === numberOfPages && nextEl) {
+                nextEl.classList.add(disabled);
             }
         };
         
-        var next = function() {
-            if (currentPage < numberOfPages){
+        const next = function() {
+            if (currentPage < numberOfPages) {
                 show(currentPage + 1);
             }
         };
         
-        var previous = function() {
+        const previous = function() {
             if (currentPage > 1) {
                 show(currentPage - 1);
             }
         };
         
         init();
-        return this;
-    }
+        return element;
+    };
     
     return new Paginator(this, options);
 };
-})(jQuery);
+
+// Alternative function-based approach (doesn't modify prototype)
+function paginate(element, options = {}) {
+    if (typeof element === 'string') {
+        element = document.getElementById(element);
+    }
+    
+    if (!element) {
+        console.error('Paginate: Element not found');
+        return null;
+    }
+    
+    return element.paginate(options);
+}
